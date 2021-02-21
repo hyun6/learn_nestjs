8143dc461ae3e2e16c4d1060a482b453182abc13
hyun6 <psymhs@gmail.com>
2021-02-21 23:37:41 +0900

    follow tutorial
     - movie entity
     - post, get test
     - https://nomadcoders.co/nestjs-fundamentals/lectures/1947

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,8 @@
+// TODO: using DB
+
+export class Movie {
+    id: number;
+    title: string;
+    year: number;
+    genres: string[];
+}
---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+
+    constructor(private readonly moviesService: MoviesService) { }
+
+    @Get()
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
+    }
+
+    @Get('search')
+    search(@Query('year') searchingYear: string) {
+        return `searching year: ${searchingYear}`;
+    }
+
+    @Get(':id')
+    getOne(@Param('id') movieId: string): Movie {
+        return this.moviesService.getOne(movieId);
+    }
+
+    @Post()
+    create(@Body() movieData) {
+        return this.moviesService.create(movieData);
+    }
+
+    @Patch(':id')
+    update(@Param('id') movieId: string, @Body() movieData) {
+        return {
+            movieId,
+            ...movieData
+        };
+    }
+
+    @Delete(':id')
+    delete(@Param('id') movieId: string) {
+        return this.moviesService.deleteOne(movieId);
+    }
+}
