63299cc900a71a9ce85cd9bf79022732b33e1b1a
hyun6 <psymhs@gmail.com>
2021-02-28 00:06:01 +0900

    (finish tutorial) add e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,99 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { CreateMovieDto } from 'src/movies/dto/create-movie.dto';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+    let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+        app = moduleFixture.createNestApplication();
+        app.useGlobalPipes(new ValidationPipe({
+            whitelist: true,
+            forbidNonWhitelisted: true,
+            transform: true
+        }));
+        await app.init();
+    });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+    it('/ (GET)', () => {
+        return request(app.getHttpServer())
+            .get('/')
+            .expect(200)
+            .expect('Welcome Home');
+    });
+
+    describe('/movies', () => {
+        it('GET 200', () => {
+            return request(app.getHttpServer())
+            .get('/movies')
+            .expect(200)
+            .expect([])
+        });
+
+        it('POST 201', () => {
+            return request(app.getHttpServer())
+            .post('/movies')
+            .send({title: 'test', year: 2021, genres: ['test']} as CreateMovieDto)
+            .expect(201);
+        });
+
+        it('POST 400', () => {
+            return request(app.getHttpServer())
+            .post('/movies')
+            .send({title: 'test', year: 2021, genres: ['test'], hack: 'hacking data'} as CreateMovieDto)
+            .expect(400);
+        });
+
+        it('DELETE 404', () => {
+            return request(app.getHttpServer())
+            .delete('/movies')
+            .expect(404);
+        });
+    });
+
+    describe('/movies/:id', () => {
+        it('GET 200', () => {
+            return request(app.getHttpServer())
+            .get('/movies/1')
+            .expect(200);
+        });
+
+        it('GET 404', () => {
+            return request(app.getHttpServer())
+            .get('/movies/999')
+            .expect(404);
+        });
+
+        it('PATCH 200', () => {
+            return request(app.getHttpServer())
+            .patch('/movies/1')
+            .send({year: 1984})
+            .expect(200);
+        });
+
+        it('PATCH 404', () => {
+            return request(app.getHttpServer())
+            .patch('/movies/999')
+            .send({year: 1984})
+            .expect(404);
+        });
+
+        it('DELETE 200', () => {
+            return request(app.getHttpServer())
+            .delete('/movies/1')
+            .expect(200);
+        });
+
+        it('DELETE 404', () => {
+            return request(app.getHttpServer())
+            .delete('/movies/999')
+            .expect(404);
+        });
+    });
 });
